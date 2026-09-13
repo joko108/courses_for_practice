8708756a15284eacdf66d3d976539d018c81c6b4
KirillZverev <zverev04@gmail.com>
2026-09-13 20:29:50 +0300

    add new comments

--- src/routes/tests.ts
+++ src/routes/tests.ts
@@ -5,6 +5,7 @@ import { HTTP_STATUSES } from "../utils";
 export const getTestsRouter = (db: DBType) => {
     const router: Router = express.Router();
 
+    // Clear database for e2e-tests
     router.delete('/data', (_req: Request, res: Response) => {
         db.courses = [];
         res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
